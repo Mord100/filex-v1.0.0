cbb38fb536743cb1054651fc7bc0f632471a27e1
Mordecai <chingolo265@gmail.com>
2024-08-25 12:03:04 +0200

    clerk signin and signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+    const session = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello World</Button>
+<SignedIn>
+      <SignOutButton>
+      <Button>Sign Out</Button>
+      </SignOutButton>
+</SignedIn>
+
+
+<SignedOut>
+  <SignInButton mode="modal">
+    <Button>Sign In</Button>
+  </SignInButton>
+</SignedOut>
     </main>
   );
 }
